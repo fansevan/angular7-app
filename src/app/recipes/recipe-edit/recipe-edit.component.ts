f7353030781bb0a70435fee1d78a8a7021e8423c
fansevan <vsevolod.fandeev@gmail.com>
2019-01-18 00:53:24 +0300

    with forms

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+
+import { RecipesService } from '../resipes.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,15 +12,82 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private recipesService: RecipesService) { }
 
   ngOnInit() {
   	this.route.params
   		.subscribe((params: Params) => {
   			this.id = +params['id'];
   			this.editMode = params['id'] != null;
+        this.initForm();
   		});
   }
 
+  onSubmit() {
+    const newRecipe = this.recipeForm.value;
+
+    if (this.editMode) {
+      this.recipesService.updateRecipe(this.id, newRecipe);
+    } else {
+      this.recipesService.addRecipe(newRecipe);
+    }
+
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients'))
+      .push(new FormGroup({
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/)
+        ]),
+      }));
+  }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescroption = '';
+    let recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe = this.recipesService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescroption = recipe.description;
+
+      if (recipe['ingredients']){
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(new FormGroup({
+            name: new FormControl(ingredient.name, Validators.required),
+            amount: new FormControl(ingredient.amount, [
+              Validators.required,
+              Validators.pattern(/^[1-9]+[0-9]*$/)
+            ]),
+          }));
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescroption, Validators.required),
+      ingredients: recipeIngredients
+    });
+  }
 }
--- src/app/recipes/resipes.service.ts
+++ src/app/recipes/resipes.service.ts
@@ -1,12 +1,15 @@
-import { EventEmitter, Injectable } from '@angular/core'
+import { Injectable } from '@angular/core'
+import { Subject } from 'rxjs'
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class RecipesService {
+	recipesChanged = new Subject<Recipe[]>();
+
 	private recipes: Recipe[] = [
 	  	new Recipe(
 	  		'Schnitzel',
@@ -38,5 +41,20 @@ export class RecipesService {
 
 	addIngredientsToShoppingList(ingredients: Ingredient[]) {
 		this.shoppingListService.addIngredients(ingredients);
+	}
+
+	addRecipe(recipe: Recipe) {
+		this.recipes.push(recipe);
+		this.recipesChanged.next(this.recipes.slice());
+	}
+
+	updateRecipe(index: number, newRecipe: Recipe) {
+		this.recipes[index] = newRecipe;
+		this.recipesChanged.next(this.recipes.slice());
+	}
+
+	deleteRecipe(index: number) {
+		this.recipes.splice(index, 1);
+		this.recipesChanged.next(this.recipes.slice());
 	}
 }
